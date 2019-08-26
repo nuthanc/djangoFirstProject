7a61ec9c2565bdb96a05ec7bd4bf7adcd299e0ca
NuthanChandra <nuthanchandra1997@gmail.com>
2019-08-26 23:22:14 +0530

    Models and poplulate

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import Response
+from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
-    return "Hello-world from django"
+    my_dict = {'thingVar': 'Hallo amigos, I am thingVar from first_app/views.py'}
+    return render(request,'first_app/index.html',context=my_dict)
